915bde3072b8679a71d66dfc306c348ea0019365
LM Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2020-11-06 21:06:34 +0100

    final dataé

--- crud.py
+++ crud.py
@@ -5,7 +5,7 @@ import schemas
 
 
 def get_players(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Player).offset(skip).limit(limit).all()
+    return db.query(models.Player).order_by(models.Player.number_of_try.desc()).offset(skip).limit(limit).all()
 
 def create_player(db: Session, player: schemas.Player):
     db_player = models.Player(name=player.name, surname=player.surname, age=player.age, height=player.height, weight=player.weight, position=player.position, nationality=player.nationality, games=player.games, number_of_try=player.number_of_try, season=player.season)
